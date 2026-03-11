971043207cb8e1a434f813949af9a0f9e569d39e
Roel Smits <roelsmits@Roels-MacBook-Pro.local>
2026-03-11 09:13:37 -0500

    fix tailwind build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,8 @@ const config: Config = {
   content: [
     "./app/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
+    "./hooks/**/*.{ts,tsx}",
     "./lib/**/*.{ts,tsx}",
-    "./hooks/**/*.{ts,tsx}"
   ],
   theme: {
     extend: {},
